f22d20a70f4e5f4d6a750dd3103fc2d295ab4b09
Meredith Jones <mj19003@gmail.com>
2021-02-04 17:31:20 -0500

    Added API call & function to filter users to table.jsx

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import "./table.css";
 import API from "../../utils/API";
-import TableData from "./tableData";
+import Data from "./Data";
 
 //Set up the table state
 class Table extends React.Component {
@@ -12,3 +12,25 @@ class Table extends React.Component {
       originalUsers: [],
       filteredUsers: [],
     };
+
+      //Setting up usersArr function to pass in state.users
+  componentDidMount() {
+    this.usersArr();
+  }
+
+  //API call to users from the random generator
+  usersArr = () => {
+    API.getUsers()
+      .then(res => this.setState({ originalUsers: res.data.results, filteredUsers: res.data.results }))
+      .catch(err => console.log(err));
+  }
+
+    //Setting up the function to filter users
+    handleChange = e => {
+        const { name, value } = e.target;
+    
+        this.setState({
+          [name]: value
+        });
+//export
+export default Table;
