173c7978a57b3d79ca38eb7d1eb78fd201e2256e
Meredith Jones <mj19003@gmail.com>
2021-02-04 17:23:42 -0500

    Added state for the table

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -3,3 +3,12 @@ import React from "react";
 import "./table.css";
 import API from "../../utils/API";
 import TableData from "./tableData";
+
+//Set up the table state
+class Table extends React.Component {
+    state = {
+      search: "",
+      className: "fas fa-arrow-square-up",
+      originalUsers: [],
+      filteredUsers: [],
+    };
