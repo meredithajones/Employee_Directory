98a71429fa09eda5c3ce92fce6c463efeccf0dd0
Meredith Jones <mj19003@gmail.com>
2021-02-04 17:55:49 -0500

    Added functions to sort by name, email, phone or city

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -55,5 +55,52 @@ class Table extends React.Component {
         this.setState({ className: "fas fa-chevron-circle-up" })
       }
     }
+
+    //function to sort users ascending or descending
+  handleSort = e => {
+    let {className, id} = e.target
+    
+    //Sort by User name
+    if(className === "fas fa-chevron-circle-up" && id === "name"){
+      const sortUsersUp = this.state.filteredUsers.sort((a, b) => (a.name.first.toLowerCase() > b.name.first.toLowerCase()) ? 1 : -1)
+      this.className(className)
+      this.setState({ filteredUsers: sortUsersUp });
+    } else if(className === "fas fa-chevron-circle-down" && id === "name"){
+      const sortUsersDown = this.state.filteredUsers.sort((a, b) => (a.name.first.toLowerCase() < b.name.first.toLowerCase()) ? 1 : -1)
+      this.className(className)
+      this.setState({ filteredUsers: sortUsersDown });
+
+      //Sort by Phone number
+    } else if(className === "fas fa-chevron-circle-up" && id === "phone"){
+      const sortUsersUp = this.state.filteredUsers.sort((a, b) => (a.phone > b.phone) ? 1 : -1)
+      this.className(className)
+      this.setState({ filteredUsers: sortUsersUp });
+    } else if(className === "fas fa-chevron-circle-down" && id === "phone"){
+      const sortUsersDown = this.state.filteredUsers.sort((a, b) => (a.phone < b.phone) ? 1 : -1)
+      this.className(className)
+      this.setState({ filteredUsers: sortUsersDown });
+
+      //Sort by email address
+    } else if(className === "fas fa-chevron-circle-up" && id === "email"){
+      const sortUsersUp = this.state.filteredUsers.sort((a, b) => (a.email > b.email) ? 1 : -1)
+      this.className(className)
+      this.setState({ filteredUsers: sortUsersUp });
+    } else if(className === "fas fa-chevron-circle-down" && id === "email"){
+      const sortUsersDown = this.state.filteredUsers.sort((a, b) => (a.email < b.email) ? 1 : -1)
+      this.className(className)
+      this.setState({ filteredUsers: sortUsersDown });
+
+      //Sort by City
+    } else if(className === "fas fa-chevron-circle-up" && id === "country"){
+      const sortUsersUp = this.state.filteredUsers.sort((a, b) => (a.location.city.toLowerCase() > b.location.city.toLowerCase()) ? 1 : -1)
+      this.className(className)
+      this.setState({ filteredUsers: sortUsersUp });
+    } else if(className === "fas fa-chevron-circle-down" && id === "country"){
+      const sortUsersDown = this.state.filteredUsers.sort((a, b) => (a.location.city.toLowerCase() < b.location.city.toLowerCase()) ? 1 : -1)
+      this.className(className)
+      this.setState({ filteredUsers: sortUsersDown });
+    }
+  }
+
 //export
 export default Table;
