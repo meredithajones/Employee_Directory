840a845db7def7b6d93fedbb06245fb97f87edda
Meredith Jones <mj19003@gmail.com>
2021-02-04 16:59:36 -0500

    Complete table data info

--- src/components/table/data.jsx
+++ src/components/table/data.jsx
@@ -6,14 +6,31 @@ function Data(props) {
         <table className= "table">
             <thead className= "thead-striped">
             <tr>
-{/* //Set up columns for Photo,Name, Phone #, Email   */}
+{/* //Set up columns for Photo,Name, Phone #, Email & city   */}
         <th scope="col">Photo</th>
             <th scope="col">Name<span onClick={props.handleSort}><i id="name" className={props.class}></i></span></th>
             <th scope="col">Phone<span onClick={props.handleSort}><i id="phone" className={props.class}></i></span></th>
             <th scope="col">Email<span onClick={props.handleSort}><i id="email" className={props.class}></i></span></th>
+            <th scope="col">City<span onClick={props.handleSort}><i id="city" className={props.class}></i></span></th>
           </tr>
         </thead>
-    
+        <tbody>
+        {props.users.map(user => (
+          <tr key={user.email}>
+            <td><img src={user.picture.medium} alt="employeePhoto" /></td>
+            <td>{user.name.first} {user.name.last}</td>
+            <td>{user.phone}</td>
+            <td>{user.email}</td>
+            <td>{user.location.city}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
     )
-    
 }
+//export 
+export default Data;
+
+    
+
+
