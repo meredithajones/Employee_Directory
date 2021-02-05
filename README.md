a641533959ad459c338ff7111859716c13b3f2ff
Meredith Jones <mj19003@gmail.com>
2021-02-04 20:26:21 -0500

    Added header

--- README.md
+++ README.md
@@ -71,6 +71,7 @@ In order to run the application, open the server.js file in integrated terminal,
 
 ## Credits
 
+* [MDN Web Docs String.prototype.localeCompare()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare)
 * [React Bootstrap Components Docs: Tables](https://react-bootstrap.github.io/components/table/)
 * [React Dev Docs: Create React App, Folder Structure](https://create-react-app.dev/docs/folder-structure/) 
 * [Mdn web docs general research](https://developer.mozilla.org/en-US/)
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,13 +3,15 @@
 import React from "react"
 import "./header.css"
 
-const Header = () => {
-    return (
-        <header>
-        <div>
-            <h1>My Header</h1>
-        </div>
-        </header>
-    )
+function Header() {
+  return (
+    <div className="jumbotron jumbotron-fluid header">
+      <div className="container">
+        <h1 className="display-4">Employee Directory</h1>
+        <p className="lead">View All Employees<br></br>Sort Employees alphebetically by First Name<br></br>or search for specific employees by name</p>
+      </div>
+    </div>
+  )
 }
+
 export default Header;
