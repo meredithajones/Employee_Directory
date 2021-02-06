bcc7cebfad8b5e79cf666d280eb16269e5f2e0a9
Meredith Jones <mj19003@gmail.com>
2021-02-06 16:23:52 -0500

    Updates to table.jsx

--- src/components/table/data.jsx
+++ src/components/table/data.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
+import './table.css';
 
 
 //Setting up the table for employee data using className(s)
@@ -9,9 +10,8 @@ function Data(props) {
             <thead className= "thead-striped">
             <tr>
 {/* //Set up columns for Photo,Name, Phone #, Email & city   */}
-
         <th scope="col">Photo</th>
-            <th onClick={() =>{props.handleSort(props.class)}} scope="col">Name <i className="fas fa-sort"></i><i id="name" className={props.class}></i></th>
+            <th onClick={() =>{props.handleSort(props.class)}}> Name <i className="fas fa-sort"></i><i id="name" class = "name" className={props.class}></i></th>
             <th scope="col">Phone<span onClick={props.handleSort}><i id="phone" className={props.class}></i></span></th>
             <th scope="col">Email<span onClick={props.handleSort}><i id="email" className={props.class}></i></span></th>
             <th scope="col">City<span onClick={props.handleSort}><i id="city" className={props.class}></i></span></th>
